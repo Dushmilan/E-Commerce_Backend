dc3fdf12ec30c6504e0f33b7f3c7e074f60cffd1
dushmilan <dushmilan03@gmail.com>
2025-10-21 21:56:52 +0530

    Installed jasonwebtoken and added utils/middleWare/middleWare.js and made changes in routing userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const userRoutes = require('express').Router();
 const userController = require('../controllers/userController');
+const middleWare = require('../utils/middleWare/middleWare');
 
 // Define user-related routes
 userRoutes.get('/', userController.getAllUsers);
 userRoutes.get('/:id', userController.getUserById);
 userRoutes.post('/', userController.createUser);
-userRoutes.put('/:id', userController.updateUser);
-userRoutes.delete('/:id', userController.deleteUser);
+userRoutes.put('/:id', middleWare.authenticateToken,userController.updateUser);
+userRoutes.delete('/:id',middleWare.authenticateToken, userController.deleteUser);
 
 
 module.exports = userRoutes;
